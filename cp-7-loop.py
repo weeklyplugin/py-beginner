84583b1eb461be258b69cf3c8f4ff3d81b05fa0e
Vasim Shaikh <vasims.magneto@gmail.com>
2020-11-16 01:29:46 +0530

    write loop with conditional statement

---
+++ cp-7-loop.py
@@ -0,0 +1,13 @@
+#loop : its simple code which iterate untill exit statement encounter.
+
+popular_website = ['google','amazon','paypal','upgrade'] 
+
+
+# if dont use loop then.
+
+
+for website in popular_website :
+    if website == 'amazon' :
+     print(website+' '+"Forest")
+    else:
+     print(website)
