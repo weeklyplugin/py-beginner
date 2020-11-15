a32fd9203878c68bf46d433e445bca9b147dc91c
Vasim Shaikh <vasims.magneto@gmail.com>
2020-11-15 23:18:04 +0530

    variable and concate

---
+++ cp-2-variables.py
@@ -0,0 +1,12 @@
+# Variable : A variable is peace of information block. Same as other language its also work
+# There is symbol to identify variable in Python.
+
+start = "Vasim Shaikh"
+finish = "Python beginner"
+
+print(start)
+print(finish)
+
+# Concate Two string using +''+ operator
+
+print(start+' '+finish)
