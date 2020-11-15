30b178620e499d8af5a6c6aaac2b924e8763b557
Vasim Shaikh <vasims.magneto@gmail.com>
2020-11-16 01:16:44 +0530

    write function and taking inputs

---
+++ cp-5-functions.py
@@ -0,0 +1,16 @@
+#function : A peace of stand alon code which written for resuablity purpose. 
+# reduce duplicate code
+
+# function without argument
+
+def wishMe():
+    print("Hi there")
+
+# function with argument
+
+def addition(a,b):
+    return a+b
+
+wishMe()
+
+print(addition(5,6))
---
+++ cp-6-taking-args.py
@@ -0,0 +1,9 @@
+# input() : This function will get value from console it self and store in variable. 
+# You need to caste before use
+
+def printMyName(name):
+    print("Your name is "+ name)
+
+person_name = input("Whats you name: ")
+
+printMyName(person_name)
